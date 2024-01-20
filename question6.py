7611147bfedca40847b0f6fa1e9b97827fc637f6
TallyGreen <127121815+TallyGreen@users.noreply.github.com>
2024-01-19 22:09:58 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,14 +1,26 @@
 def question6_1():
     """
     answer = {
-        'equal_width': ['string', integer]
-        'equal_frequency': ['string', integer]
+        'equal_width': ['Change', 3, 5, 10]
+        'equal_frequency': ['Change', 3, 5, 10]
     }
     """
     return answer
 
 def question6_2():
+    """
+    answer = {
+        'equal_width': ['Change', 3, 5, 8, 10]
+        'equal_frequency': ['Change', 1, 2, 3, 4, 5, 7, 8, 10]
+    }
+    """
     return answer
 
 def question6_3():
+    """
+    answer = {
+        'equal_width': ['Change', 1,2,3,4,5,6,7,8,9,10]
+        'equal_frequency': ['Change', 1,2,3,4,5,6,7,8,9,10]
+    }
+    """
     return answer 
