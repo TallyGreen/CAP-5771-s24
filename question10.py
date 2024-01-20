93738633f08e13f946494fb4e0b70e72b6482829
TallyGreen <127121815+TallyGreen@users.noreply.github.com>
2024-01-19 22:17:44 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,32 +1,59 @@
 def question10_1():
     """
-    answer: python bool (True or False)
+    answer: python bool (True), Noise is not inherently a problem with count data;
     """
     return answer
 
 def question10_2():
+    """
+    answer: python bool (True)
+    """
     return answer
 
 def question10_3():
+    """
+    answer: python bool (False)
+    """
     return answer
 
 def question10_4():
+    """
+    answer: python bool (False)
+    """
     return answer
 
 def question10_5():
+    """
+    answer: python bool (False)
+    """
     return answer
 
 def question10_6():
+    """
+    answer: python bool (False)
+    """
     return answer
 
 def question10_7():
+    """
+    answer: python bool (False)
+    """
     return answer
 
 def question10_8():
+    """
+    answer: python bool (False)
+    """
     return answer
 
 def question10_9():
+    """
+    answer: python bool (False)
+    """
     return answer
 
 def question10_10():
+    """
+    answer: python bool (False)
+    """
     return answer
