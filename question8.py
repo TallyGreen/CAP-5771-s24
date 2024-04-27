3d3d55dd110b9be56458ac08d57e4737229ed074
TallyGreen <127121815+TallyGreen@users.noreply.github.com>
2024-04-27 19:30:36 -0400

    Update question8.py

--- question8.py
+++ question8.py
@@ -1,23 +1,15 @@
 def question8_1():
-    """
     answer=  ['3/5', '0']
-    """
     return answer
 
 def question8_2():
-    """
     answer=  ['3/5', '0']
-    """
     return answer
 
 def question8_3():
-    """
     answer= 'equally similar'
-    """    
     return answer 
 
 def question8_4():
-    """
     answer= 'equally similar'
-    """    
     return answer 
