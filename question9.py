7999b68078455bceeef1ffc48f5aade79f389d88
TallyGreen <127121815+TallyGreen@users.noreply.github.com>
2024-04-27 19:31:50 -0400

    Update question9.py

--- question9.py
+++ question9.py
@@ -1,29 +1,19 @@
 def question9_1():
-    """
     answer = 'SMC'
-    """
     return answer
 
 def question9_2():
-    """
     answer = 'SMC'
-    """
     return answer
 
 def question9_3():
-    """
-    answer = 'Euclidean distance'
-    """
+    answer = 'Euclidean'
     return answer
 
 def question9_4():
-    """
-    answer = 'Euclidean distance'
-    """
+    answer = 'Euclidean'
     return answer
 
 def question9_5():
-    """
     answer = 'cosine similarity'
-    """
     return answer
