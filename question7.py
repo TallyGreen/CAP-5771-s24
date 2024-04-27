f9628b63d41aaff54bf0a71c82e0ae8d0839594e
TallyGreen <127121815+TallyGreen@users.noreply.github.com>
2024-04-27 19:29:16 -0400

    Update question7.py

--- question7.py
+++ question7.py
@@ -1,19 +1,11 @@
 def question7_1():
-    """
-    answer = 'string'
-    """
+    answer = 'increase/decrease'
     return answer
 
 def question7_2():
-    """
-    answer = 'string'
-    """
+    answer = 'non-increasing'
     return answer
 
 def question7_3():
-    """
-    answer = [(b1, e1), (b2, e2), ..., (bn, en)]
-    The representation of inifinity is 'infinity'. 
-    bi, ei are floats or 'infinity'. 
-    """
+    answer = [('-infinity', 3.0), (3.0, 4.0), (4.0, 5.0), (5.0, 'infinity')]
     return answer 
